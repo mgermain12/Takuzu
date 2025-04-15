d29bf1ac4ab69ba152f7392c70378d25043b5ab5
mgermain12 <marine.germain@etu.umontpellier.fr>
2025-04-15 16:16:57 +0200

    fix bug

--- R/jouer.R
+++ R/jouer.R
@@ -2,13 +2,5 @@
 #'
 #' @export
 jouer <- function() {
-  library(shiny)
-  library(shinyjs)
-  library(bslib)
-
-  source(system.file("R", "niveau.R", package = "Takuzu"), local = TRUE)
-  source(system.file("R", "grille.R", package = "Takuzu"), local = TRUE)
-  source(system.file("R", "verification.R", package = "Takuzu"), local = TRUE)
-
-  runApp(system.file("R", "code.R", package = "Takuzu"))
+  shiny::runApp(system.file("app", package = "Takuzu"))
 }
